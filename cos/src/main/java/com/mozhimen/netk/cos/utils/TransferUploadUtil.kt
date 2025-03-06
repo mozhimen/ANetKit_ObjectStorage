a617263413fd0b78dc06d15bf66b03e140b9e013
mozhimen <58215586+mozhimen@users.noreply.github.com>
2025-03-06 21:31:23 +0800

    250306

--- cos/src/main/java/com/mozhimen/netk/cos/utils/TransferUploadUtil.kt
+++ cos/src/main/java/com/mozhimen/netk/cos/utils/TransferUploadUtil.kt
@@ -13,6 +13,8 @@ import com.tencent.cos.xml.model.CosXmlResult
 import com.tencent.cos.xml.transfer.COSXMLUploadTask.COSXMLUploadTaskResult
 import com.tencent.cos.xml.transfer.TransferConfig
 import com.tencent.cos.xml.transfer.TransferManager
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
 
 
 /**
@@ -22,18 +24,26 @@ import com.tencent.cos.xml.transfer.TransferManager
  * @Date 2023/12/22
  * @Version 1.0
  */
-fun CosXmlService.transferUploadFile(
-    bucket: String,
-    cosPath: String,
-    strFilePathName: String,
-    onComplete: IA_Listener<Boolean>,
-    onProgress: IAB_Listener<Long, Long>? = null
-) {
-    TransferUploadUtil.transferUploadFile(this, bucket, cosPath, strFilePathName, onComplete, onProgress)
-}
+//fun CosXmlService.transferUploadFile(
+//    bucket: String,
+//    cosPath: String,
+//    strFilePathName: String,
+//    onComplete: IA_Listener<Boolean>,
+//    onProgress: IAB_Listener<Long, Long>? = null
+//) {
+//    TransferUploadUtil.transferUploadFile(this, bucket, cosPath, strFilePathName, onComplete, onProgress)
+//}
 
 object TransferUploadUtil : BaseUtilK() {
     private val _transferConfig by lazy { TransferConfig.Builder().build() }// 初始化 TransferConfig，这里使用默认配置，如果需要定制，请参考 SDK 接口文档
+
+    @JvmStatic
+    suspend fun transferUploadFile_onBack(cosXmlService: CosXmlService, bucket: String, cosPath: String, strFilePathName: String, onProgress: IAB_Listener<Long, Long>? = null): Boolean =
+        suspendCancellableCoroutine { continuation ->
+            transferUploadFile(cosXmlService, bucket, cosPath, strFilePathName, { res ->
+                continuation.resume(res)
+            }, onProgress)
+        }
 
     /**
      * 高级接口上传对象
@@ -45,7 +55,7 @@ object TransferUploadUtil : BaseUtilK() {
         cosPath: String,/*//对象在存储桶中的位置标识符，即称对象键*/
         strFilePathName: String,//本地文件的绝对路径
         onComplete: IA_Listener<Boolean>,
-        onProgress: IAB_Listener<Long, Long>? = null
+        onProgress: IAB_Listener<Long, Long>? = null,
     ) {
         val transferManager = TransferManager(cosXmlService, _transferConfig)// 初始化 TransferManager
         val uploadId: String? = null//若存在初始化分块上传的 UploadId，则赋值对应的 uploadId 值用于续传；否则，赋值 null
@@ -68,7 +78,7 @@ object TransferUploadUtil : BaseUtilK() {
             override fun onFail(
                 request: CosXmlRequest,
                 clientException: CosXmlClientException?,
-                serviceException: CosXmlServiceException?
+                serviceException: CosXmlServiceException?,
             ) {
                 clientException?.printStackTrace() ?: serviceException?.printStackTrace()
                 UtilKLogWrapper.w(TAG, "onFail: ")
